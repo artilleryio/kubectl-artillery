f68bbb897208442e077733ea387b3331ba7e98ed
Ezo Saleh <ezo@the-experimenters.com>
2022-05-11 17:32:08 +0100

    Tidy up unused values.

--- cmd/kubectl-artillery/main.go
+++ cmd/kubectl-artillery/main.go
@@ -31,12 +31,6 @@ const (
 
 	// WorkerImage the Artillery image used by workers to run load tests.
 	WorkerImage = "artilleryio/artillery:latest"
-
-	// TestScriptVol the volume used by created LoadTest Pods to load the test script ConfigMap.
-	TestScriptVol = "test-script"
-
-	// TestScriptFilename expected filename used by the test script ConfigMap.
-	TestScriptFilename = "test-script.yaml"
 )
 
 // kubectl-artillery CLI entrypoint
